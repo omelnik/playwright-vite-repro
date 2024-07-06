3f46a04f39f8a94fe91dd72b38f9aeb3f3defa66
Nikita Omelchenko <nikomel@Nikitas-MacBook-Pro.local>
2024-07-06 09:53:11 -0700

    (feat): add dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 import { BASE_URL as baseURL } from "./src/constants";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: ".env" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
